9a0ba0ab6d4c793095660fc0cd7dea9b483353c6
RyanSleszynski <rsleszynski1@pride.hofstra.edu>
2022-03-23 18:42:45 -0400

    - added plug-ins for csv files

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import login
 
 # In this project I want to try and create a simple login application that will allow you to
 # create a new account, delete or deactivate an account, login, and ability to change anything in the profile.
+with open('accounts.csv', 'r+') as accounts:
+    accounts.write()
 
-accounts = open("accounts.txt", "a")
 
 user = login.Account('Slinkydo', 'Quantum99', 'Ryan', 'Sleszynski', 'sleszynski.ryan@gmail.com', '516-557-8823' ,'')
 accounts.write(user)
