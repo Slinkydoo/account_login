d56b21e6c17f9529e5d7bcd407529198971bed3b
RyanSleszynski <rsleszynski1@pride.hofstra.edu>
2022-03-23 19:06:02 -0400

    - playing with reading a writing to files

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import csv
+
 import login
 
 # In this project I want to try and create a simple login application that will allow you to
 # create a new account, delete or deactivate an account, login, and ability to change anything in the profile.
-with open('accounts.csv', 'r+') as accounts:
-    accounts.write()
-
-
-user = login.Account('Slinkydo', 'Quantum99', 'Ryan', 'Sleszynski', 'sleszynski.ryan@gmail.com', '516-557-8823' ,'')
-accounts.write(user)
-print(accounts)
+# user = login.Account('Slinkydo', 'Quantum99', 'Ryan', 'Sleszynski', 'sleszynski.ryan@gmail.com', '516-557-8823' ,'')
+# with open('accounts_list.csv', 'a') as accounts:
+    # accounts.write(user.username, user.password, user.first_name, user)
+user = csv.reader('accounts_list.csv')
+print(user)
+# accounts.write(user)
+# print(accounts)
